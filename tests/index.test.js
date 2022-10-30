08601a060b22d1260bd7d3e6bcbfbac4fc503f7e
rayanaziz-bac <49303647+Arex360@users.noreply.github.com>
2022-10-30 19:46:33 +0500

    Safe mode

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,6 @@
 import Home from "../pages/index";
 import "@testing-library/jest-dom";
 import { fireEvent, render, screen } from "@testing-library/react";
+describe("Calculator", () => {
+   
+  });
